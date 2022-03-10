0b2bc3466617f9fadf6554e977f0bae36deaeb67
QuachSon1995 <61349661+QuachSon1995@users.noreply.github.com>
2022-03-10 17:41:14 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded( {extended: true} ))
+
+
+app.get("/", (req,res)=>{
+    return res.send({message:"abc"})
+});
+
+
+app.listen(5000, () => {
+    console.log(`Server running at ${process.env.PORT}`);
+});
